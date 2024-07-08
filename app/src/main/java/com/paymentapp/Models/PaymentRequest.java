bec39f45bab9b7be55550a60d6e035058fa11ed5
MwansaMukonko <mwansa.mukonko@qualisdigital.co>
2024-07-08 14:50:34 +0200

    Amount Type Big Decimal to int (How ever caution needs to be made in future)

--- app/src/main/java/com/paymentapp/Models/PaymentRequest.java
+++ app/src/main/java/com/paymentapp/Models/PaymentRequest.java
@@ -14,15 +14,78 @@ public class PaymentRequest {
     private String customerLastName;
     private String email;
     private String phoneNumber;
-    private BigDecimal amount;
+    private int amount;
 
-    public PaymentRequest(String description, String customerFirstName, String customerLastName, String email, String phoneNumber, BigDecimal amount) {
+    public PaymentRequest(String description, String customerFirstName, String customerLastName, String email, String phoneNumber, int amount) {
         this.description = description;
         this.customerFirstName = customerFirstName;
         this.customerLastName = customerLastName;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.amount = amount;
+    }
+
+    public PaymentRequest() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCustomerFirstName() {
+        return customerFirstName;
+    }
+
+    public void setCustomerFirstName(String customerFirstName) {
+        this.customerFirstName = customerFirstName;
+    }
+
+    public String getCustomerLastName() {
+        return customerLastName;
+    }
+
+    public void setCustomerLastName(String customerLastName) {
+        this.customerLastName = customerLastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "PaymentRequest{" +
+                "description='" + description + '\'' +
+                ", customerFirstName='" + customerFirstName + '\'' +
+                ", customerLastName='" + customerLastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", amount=" + amount +
+                '}';
     }
 }
 
