e8aebb51f0a8966f64798654c729fe55b5facba7
MwansaMukonko <mwansa.mukonko@qualisdigital.co>
2024-07-08 14:52:04 +0200

    Activity Flow an execution mjr

--- app/src/main/java/com/paymentapp/PaymentCheckout/Paycheckout.java
+++ app/src/main/java/com/paymentapp/PaymentCheckout/Paycheckout.java
@@ -6,8 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.paymentapp.Helpers.CollectionHelper;
+import com.paymentapp.LocalStorage.SharedPrefs;
 import com.paymentapp.Models.PaymentRequest;
 import com.paymentapp.Models.SmsRequest;
 import com.paymentapp.R;
@@ -17,32 +19,34 @@ import java.math.BigDecimal;
 public class Paycheckout extends AppCompatActivity {
     private EditText customerFirstName, customerLastName, email, phoneNumber;
     private Button btn;
-    private final BigDecimal pay = BigDecimal.valueOf(5.00);
+    private final int pay = 5;
     private final String description = "Monthly Subscription";
     private final CollectionHelper collectionHelper = new CollectionHelper();
     private final String sender = "MyCompany";
     private final String message = "My Message";
     private final String recipient = "ContactNumber";
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paycheckout);
 
+        //Add a text Violation Class to validate the data coming from the edit text here ignore at your own risk
         customerFirstName = findViewById(R.id.customerFirstName);
         customerLastName = findViewById(R.id.customerLastName);
         email = findViewById(R.id.email);
         phoneNumber = findViewById(R.id.phoneNumber);
         btn = findViewById(R.id.payButton);
 
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                collectionHelper.generateToken(Paycheckout.this);
-                collectionHelper.requestPayment(Paycheckout.this, new PaymentRequest(description,customerFirstName.toString(), customerLastName.toString(), email.toString(),phoneNumber.toString(),pay));
-                collectionHelper.getPaymentStatus(Paycheckout.this);
-                //Trigger This for your sms
+                PaymentRequest paymentRequest = new PaymentRequest(description,customerFirstName.getText().toString(),customerLastName.getText().toString(),email.getText().toString(),phoneNumber.getText().toString(),pay);
+                collectionHelper.generateToken(Paycheckout.this, paymentRequest);
+//                collectionHelper.requestPayment(Paycheckout.this, new PaymentRequest(description,customerFirstName.toString(), customerLastName.toString(), email.toString(),phoneNumber.toString(),pay));
+//                collectionHelper.getPaymentStatus(Paycheckout.this);
+//                //Trigger This for your sms
 //                collectionHelper.sendSms(Paycheckout.this, new SmsRequest(sender,message,recipient));
             }
         });
